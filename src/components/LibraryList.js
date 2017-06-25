b81eecdc192c8d439f010bdd40cad37bf1d0908f
Adam Pasternack <adam.pasternack@gmail.com>
2017-06-25 11:13:26 -0400

    maping redux state to components

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -7,4 +7,11 @@ class LibraryList extends Component {
     }
 }
 
-export default connect()(LibraryList);  //connect() returns a function that we then pass LibraryList into as we invoke it
+const mapToStateProps = state => {
+    // Whatever is returned here is passed as a prop named 'libraries',
+    // with a value of state.libraries to LibraryList
+    return { libraries: state.libraries };
+};
+
+// connect() returns a function that we then pass LibraryList into as we invoke it
+export default connect(mapToStateProps)(LibraryList);
