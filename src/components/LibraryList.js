453ceed6afee5905bca454c6f02aa756c47cba36
Adam Pasternack <adam.pasternack@gmail.com>
2017-06-26 15:04:47 -0400

    getting the data for ListView component

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+    componentWillMount() {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.DataSource = ds.cloneWithRows(this.props.libraries);
+    }
+    
     render() {
         console.log(this.props);
         return;
